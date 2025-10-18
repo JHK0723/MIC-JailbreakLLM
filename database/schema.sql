b52919c58fe3ab9c2d0572c2b05bb1c41cc70f56
anvisgit <anvi.wadhwa2024@vitstudent.ac.in>
2025-10-18 11:28:45 +0530

    .

--- database/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE teams (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    team_name TEXT UNIQUE NOT NULL,
-    overall_time_sec REAL NOT NULL,
-    prompt1 TEXT,
-    prompt2 TEXT,
-    prompt3 TEXT,
-    prompt4 TEXT
-);
